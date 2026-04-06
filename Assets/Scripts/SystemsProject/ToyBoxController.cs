f6b5875e03debf4eb41b48587690d4eabf99606b
Ryan Chen <ryan.w.jc9@gmail.com>
2026-04-06 19:50:11 -0400

    challenge 1 completed

---
+++ Assets/Scripts/SystemsProject/ToyBoxController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+
+public class ToyBoxController : MonoBehaviour
+{
+    public UnityEvent OnRelease; //event to be hooked up into the inspector
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //challenge 1: single input gate
+    public void OnWind(InputAction.CallbackContext context)
+    {
+        //only trigger when button is released
+        if (context.canceled)
+        {
+            Debug.Log("Gate passed!"); 
+            OnRelease.Invoke(); //calls all modular stuff
+        }
+
+    }
+}
