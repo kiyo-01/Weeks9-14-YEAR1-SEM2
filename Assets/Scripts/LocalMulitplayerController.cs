9097f739940183734944a04f1b62adc60eff3f21
cheryanj <cheryanj@OA-S141-17>
2026-04-07 11:42:40 -0400

    first two nodes complete, trails are NOT cooperating :(

--- Assets/Scripts/LocalMulitplayerController.cs
+++ Assets/Scripts/LocalMulitplayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,9 +8,15 @@ public class LocalMulitplayerController : MonoBehaviour
     public LocalMultiplayerManager manager;
 
     public Vector2 movementInput;
-    public float speed = 5;
+    public float speed = 5f;
 
-    // Update is called once per frame
+    public Coroutine activeRoutine;
+    public AnimationCurve curve;
+    public float aDuration = 0.5f;
+
+    public AudioSource smack;
+
+    public TrailRenderer trail;
     void Update()
     {
         transform.position += (Vector3)movementInput * speed * Time.deltaTime;
@@ -20,12 +27,58 @@ public class LocalMulitplayerController : MonoBehaviour
         movementInput = context.ReadValue<Vector2>();
     }
 
+
+
     public void OnAttack(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            Debug.Log("Player " + playerInput.playerIndex + ": Attacking!");
             manager.PlayerAttacking(playerInput);
+            Debug.Log("ATTACKING");
+
+            
+            if (activeRoutine != null) StopCoroutine(activeRoutine);
+            activeRoutine = StartCoroutine(AttackRoutine());
         }
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+            if(activeRoutine != null) StopCoroutine(activeRoutine);
+            activeRoutine = StartCoroutine(DashRoutine()); 
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        float elapsed = 0f;
+        Vector3 initialScale = Vector3.one;
+
+        smack.Play();
+        while (elapsed < aDuration)
+        {
+            elapsed += Time.deltaTime;
+            float scaleMultiplier = curve.Evaluate(elapsed / aDuration);
+            //transform.localScale = initialScale * (1f + scaleMultiplier);
+            transform.localScale = new Vector3(initialScale.x, initialScale.y * (1f - scaleMultiplier), initialScale.z);
+            yield return null;
+        }
+    }    
+
+    IEnumerator DashRoutine()
+    {
+        float t = 0f;
+        while (t < 1)
+        {
+            speed = 20f;
+            trail.emitting = true;
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f); //dash duration
+
+        speed = 5f;
+        t = 0f;
+        trail.emitting = false;
     }
 }
